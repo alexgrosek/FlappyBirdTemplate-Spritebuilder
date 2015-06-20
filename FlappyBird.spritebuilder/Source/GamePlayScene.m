f07de081a65f209febdce28a00589f14e3e92fbf
Anthony Grosek <AlexanderGrosek1@Anthonys-MacBook-Pro.local>
2015-06-20 04:10:59 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,7 @@
 - (void)initialize
 {
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     // will load a new Character from the CCB file we created for you named Character.
@@ -16,7 +17,22 @@
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    /* this will be run every frame.
+     * delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+     * put update code here
+     */
+    // Increment the time since last Obstacle was added
+    timeSinceObstacle += delta; // delta is approx. 1/60 of a second
+    
+    // check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // add a new obstacle
+        [self addObstacle];
+        
+        // then reset the timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 /* To declare a method in Objective-C, follow the following format:
  * -(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
